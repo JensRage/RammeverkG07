59e0a64bf0b6baf61bca206e12ea64201a6bc97a
JensHR <jenshrage@gmail.com>
2020-02-25 23:58:16 +0100

    Added Controls. This class will be used for handling the controlls for all player controlled objects

---
+++ src/no/hiof/G07/Controls.java
@@ -0,0 +1,22 @@
+package no.hiof.G07;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Controls implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}
