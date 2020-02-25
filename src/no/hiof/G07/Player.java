f03d8aa60e566fcb90d18573d9771d77e2d3d9c6
JensHR <jenshrage@gmail.com>
2020-02-25 23:57:27 +0100

    Added Player. This class will be used for the player controlled GameObject.

---
+++ src/no/hiof/G07/Player.java
@@ -0,0 +1,18 @@
+package no.hiof.G07;
+
+public class Player extends Unit {
+
+    public Player(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+}
