df94a90c09f96800d6672ec04abdb09fc20f478a
JensHR <jenshrage@gmail.com>
2020-02-25 23:47:45 +0100

    Added GameObject. Abstract class. Base for all future objects in the game.

---
+++ src/no/hiof/G07/GameObject.java
@@ -0,0 +1,45 @@
+package no.hiof.G07;
+
+public abstract class GameObject {
+    private int x;
+    private int y;
+    private int velocityX;
+    private int velocityY;
+
+
+
+    public abstract void tick();
+    public abstract void render();
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(int velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public int getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(int velocityY) {
+        this.velocityY = velocityY;
+    }
+}
