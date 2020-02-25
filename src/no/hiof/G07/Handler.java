d630aacad5483723632fbd4eb2de83cfa7da68e0
JensHR <jenshrage@gmail.com>
2020-02-26 00:01:46 +0100

    Added Handler. This class is of huge importance as it is responisble for making sure all objects are updated doing what they are supposed to for each tick. Example: Adding a units velocity to its current position.

---
+++ src/no/hiof/G07/Handler.java
@@ -0,0 +1,20 @@
+package no.hiof.G07;
+
+import java.util.ArrayList;
+
+public class Handler {
+    private ArrayList<GameObject> objects;
+
+    public void tick(){
+
+    }
+
+    public void render(){
+
+    }
+
+    public void addObject(GameObject object){
+        objects.add(object);
+    }
+
+}
