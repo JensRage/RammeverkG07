6273d88dd99ba927c6285a454fffc0e2efd5d203
JensHR <jenshrage@gmail.com>
2020-04-01 09:20:56 +0200

    Developed Handler class. Complete with addObject, removeObject, tick and render. render() might be missing a graphics parameter.

--- src/no/hiof/G07/Handler.java
+++ src/no/hiof/G07/Handler.java
@@ -5,16 +5,29 @@ import java.util.ArrayList;
 public class Handler {
     private ArrayList<GameObject> objects;
 
-    public void tick(){
+    public Handler() {
+        this.objects = new ArrayList<>();
+    }
 
+    public void tick(){
+        for (GameObject object : objects) {
+            object.tick();
+        }
     }
 
     public void render(){
-
+        for (GameObject object : objects) {
+            object.render();
+        }
     }
 
     public void addObject(GameObject object){
         objects.add(object);
     }
 
+    //Might remove return. But it might be good for debugging
+    public GameObject removeObject(GameObject object) {
+        objects.remove(object);
+        return object;
+    }
 }
