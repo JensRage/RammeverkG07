fed2c0c13e0070cfb70637ec617f577e3c8cac19
jenshr <jenshrage@gmail.com>
2020-04-02 22:58:18 +0200

    Added JavaDoc Still missing for keyPressed() as it currently has no logic.

--- src/src/main/java/no/hiof/G07/Controls.java
+++ src/src/main/java/no/hiof/G07/Controls.java
@@ -8,6 +8,14 @@ import java.awt.event.KeyEvent;
 import java.lang.reflect.Method;
 import java.util.Set;
 
+/**
+ * This class is designed to be the core KeyListener for our framework
+ * It holds all methods flagged with @KeyListen and loops through them when a key is pressed
+ * @see java.awt.event.KeyAdapter
+ * @see org.reflections.Reflections
+ * @author emilyhbh (Emily Healey)
+ * @version 0.1
+ */
 public class Controls extends KeyAdapter {
 
     // Finds all methods annotated with @KeyListen and calls them
@@ -19,6 +27,14 @@ public class Controls extends KeyAdapter {
 
     }
 
+    /**
+     * This method is called whenever a key is pressed.
+     * It will iterate though all methods flagged with @KeyListen and execute those.
+     * @param e     The KeyEvent that triggered the function.
+     * @throws java.lang.IllegalAccessException             If the underlying method is inaccessible.
+     * @throws java.lang.reflect.InvocationTargetException  If the called method calls an exception.
+     * {@inheritDoc}
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         for (Method method : methods) {
