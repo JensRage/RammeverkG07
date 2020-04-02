4fab3993289e59a5592f5eefc8c762a4f988b62d
jenshr <jenshrage@gmail.com>
2020-04-02 22:57:27 +0200

    Added JavaDoc

--- src/src/main/java/no/hiof/G07/Behaviour.java
+++ src/src/main/java/no/hiof/G07/Behaviour.java
@@ -1,9 +1,22 @@
 package no.hiof.G07;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+/**
+ * This class is designed to hold all the different AI states for a GameObject that needs behaviour logic executed
+ * @author jenshr (Jens Henrik Rage)
+ * @version 0.1
+ */
 public abstract class Behaviour {
 
+    /*
     private ArrayList<State> states;
+    private Enum stateNames;
+    private State currentState
+     */
+
+    private HashMap<Enum, State> states;
+    private State currentState;
 
 }
