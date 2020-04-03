32e7a33f01890e2c15183e136acb84d33c0588b8
jenshr <jenshrage@gmail.com>
2020-04-03 13:56:19 +0200

    Added JavaDocs

--- src/src/main/java/no/hiof/G07/Unit.java
+++ src/src/main/java/no/hiof/G07/Unit.java
@@ -2,18 +2,37 @@ package no.hiof.G07;
 
 import java.awt.*;
 
+/**
+ * Class Unit
+ * This class is the first successor of GameObject. It is the most basic form of instance-able GameObjects
+ * @author jenshr (Jens Rage)
+ * @version 0.1
+ */
 public class Unit extends GameObject {
 
     public Unit(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * This implementation of tick allows for steady and smooth movement,
+     * by setting XY coordinates to: it's position + its velocity at the given axis.
+     * @param delta     delta consists of how long the loop update took divided by OPTIMAL_TIME which is a double
+     *                  that describes how long each update should take.
+     *                  This variable is used to update GameObjects variables in a way that keeps the different
+     * {@inheritDoc}
+     */
     @Override
     public void tick(double delta) {
         setX(getX() + getVelocityX());
         setY(getY() + getVelocityY());
     }
 
+    /**
+     *
+     * @param g         Graphics2D object, which Canvas uses to render graphics in our window.
+     * {@inheritDoc}
+     */
     @Override
     public void render(Graphics2D g) {
 
